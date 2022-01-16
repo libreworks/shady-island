482d67edda43a6938bef7c49fccfef1fa0f8cd41
Jonathan Hawk <jonathan@hawk.ninja>
2022-01-15 23:47:11 -0500

    chore: project customization

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,32 @@
-const { awscdk } = require('projen');
+const { awscdk, ProjectType } = require("projen");
+
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Jonathan Hawk',
-  authorAddress: 'jonathan@hawk.ninja',
-  cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
-  name: 'shady-island',
-  repositoryUrl: 'git@github.com:libreworks/shady-island.git',
+  name: "shady-island",
+  projectType: ProjectType.LIB,
+  description: "Utilities and constructs for the AWS CDK",
+  authorName: "LibreWorks Contributors",
+  authorUrl: "https://github.com/libreworks/shady-island/contributors",
+  authorOrganization: true,
+  defaultReleaseBranch: "main",
+  repositoryUrl: "git@github.com:libreworks/shady-island.git",
+  homepage: "https://github.com/libreworks/shady-island",
+  bugsUrl: 'https://github.com/libreworks/shady-island/issues',
+  prettier: true,
+  compat: true,
+
+  stability: 'experimental',
+  cdkVersion: "2.1.0",
+
+  depsUpgradeOptions: {
+    workflowOptions: {
+      labels: ['auto-approve'],
+    },
+  },
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
 project.synth();
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="classes"></a>
+
+### Hello <a name="shady-island.Hello" id="shadyislandhello"></a>
+
+#### Initializers <a name="shady-island.Hello.Initializer" id="shadyislandhelloinitializer"></a>
+
+```typescript
+import { Hello } from 'shady-island'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| [`sayHello`](#shadyislandhellosayhello) | *No description.* |
+
+---
+
+##### `sayHello` <a name="shady-island.Hello.sayHello" id="shadyislandhellosayhello"></a>
+
+```typescript
+public sayHello()
+```
+
+
+
+
+
