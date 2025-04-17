ab6e139c0933765322abdf775dac2fc732b61c3f
Jonathan Hawk <jonathan@hawk.ninja>
2025-04-17 15:02:47 -0400

    feat: add UpdateRoute53AddOn (#163)

--- src/configuration/addons.ts
+++ src/configuration/addons.ts
@@ -1,5 +1,7 @@
 import { Lazy } from "aws-cdk-lib";
 import type { IFileSystem } from "aws-cdk-lib/aws-efs";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+import type { IHostedZone } from "aws-cdk-lib/aws-route53";
 import type { IBucket } from "aws-cdk-lib/aws-s3";
 import { ShellCommands } from "./commands";
 import type { IFirewallRules } from "./firewall";
@@ -114,7 +116,7 @@ export class ElasticFileSystemAddOn implements IStarterAddOn {
    * - Mount the NFS filesystem to the mount point
    * - Optionally change the mode or ownership of the mount point
    *
-   * This visitor also configures the Security Groups on both ends.
+   * This add-on also configures the Security Groups on both ends.
    *
    * @param filesystem - The elastic filesystem to mount
    * @param destination - The directory to use as the mount point
@@ -158,6 +160,127 @@ export class ElasticFileSystemAddOn implements IStarterAddOn {
         this.destination
       ),
       ...postMount
+    );
+  }
+}
+
+/**
+ * Constructor properties for UpdateRoute53AddOn.
+ */
+export interface UpdateRoute53AddOnProps extends SinglePriorityProps {
+  /**
+   * Whether to create/update an "A" record for the instance.
+   *
+   * @default - true
+   */
+  readonly ipv4?: boolean;
+  /**
+   * Whether to create/update an "AAAA" record for the instance.
+   *
+   * @default - false
+   */
+  readonly ipv6?: boolean;
+}
+
+/**
+ * An add-on that updates Route 53 with instance public-facing IP addresses.
+ *
+ * This add-on also configures the necessary IAM policy.
+ */
+export class UpdateRoute53AddOn implements IStarterAddOn {
+  /**
+   * An add-on that updates Route 53 with instance public-facing IP addresses.
+   *
+   * This add-on also configures the necessary IAM policy.
+   *
+   * @param zone - The Route 53 hosted zone
+   * @param subdomain - The subdomain of the DNS record
+   * @param props - Optional configuration properties
+   */
+  public constructor(
+    private readonly zone: IHostedZone,
+    private readonly subdomain: string,
+    private readonly props?: UpdateRoute53AddOnProps
+  ) {}
+
+  public configure(starter: Starter): void {
+    const { ipv4 = true, ipv6 = false, priority = 5 } = this.props || {};
+
+    if (!ipv4 && !ipv6) {
+      return;
+    }
+
+    const recordTypes = [];
+    if (ipv4) {
+      recordTypes.push("A");
+    }
+    if (ipv6) {
+      recordTypes.push("AAAA");
+    }
+
+    const fqdn = `${this.subdomain}.${this.zone.zoneName}`;
+    starter.grantPrincipal.addToPrincipalPolicy(
+      new PolicyStatement({
+        actions: ["route53:changeResourceRecordSets"],
+        resources: [this.zone.hostedZoneArn],
+        conditions: {
+          "ForAllValues:StringLike": {
+            "route53:ChangeResourceRecordSetsNormalizedRecordNames": [fqdn],
+          },
+          "ForAllValues:StringEquals": {
+            "route53:ChangeResourceRecordSetsActions": ["UPSERT"],
+            "route53:ChangeResourceRecordSetsRecordTypes": recordTypes,
+          },
+        },
+      })
+    );
+
+    const commands = [
+      'metaToken=$(curl -sX PUT "http://instance-data/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 300")',
+      'metaTokenHeader="X-aws-ec2-metadata-token: $metaToken"',
+      "IMD=http://instance-data/latest/meta-data",
+      'IMDNIM="$IMD/network/interfaces/macs"',
+      'myNetworkMac=$(curl -sH "$metaTokenHeader" "$IMDNIM/")',
+      'IMDNIMC="$IMDNIM/$myNetworkMac"',
+    ];
+
+    const changeBatchChanges = [];
+    if (ipv4) {
+      commands.push(
+        'myHostIpPublic=$(curl -sH "$metaTokenHeader" "${IMDNIMC}public-ipv4s")'
+      );
+      changeBatchChanges.push({
+        Action: "UPSERT",
+        ResourceRecordSet: {
+          Name: fqdn,
+          Type: "A",
+          TTL: 300,
+          ResourceRecords: [{ Value: "'$myHostIpPublic'" }],
+        },
+      });
+    }
+    if (ipv6) {
+      commands.push(
+        'myHostIpv6=$(curl -sH "$metaTokenHeader" "${IMDNIMC}ipv6s")'
+      );
+      changeBatchChanges.push({
+        Action: "UPSERT",
+        ResourceRecordSet: {
+          Name: fqdn,
+          Type: "AAAA",
+          TTL: 300,
+          ResourceRecords: [{ Value: "'$myHostIpv6'" }],
+        },
+      });
+    }
+    const changeBatch = { Changes: changeBatchChanges };
+
+    starter.addScript(
+      priority,
+      ...commands,
+      `aws route53 change-resource-record-sets --hosted-zone-id ${
+        this.zone.hostedZoneId
+      } --change-batch '${JSON.stringify(changeBatch)}'`
     );
   }
 }
